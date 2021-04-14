4f1efc292b169b62fd033f294e70ccbb5d965b11
Roosevelt <LarkinsRo@gmail.com>
2021-04-14 06:40:18 -0700

    plz work

---
+++ app.js
@@ -0,0 +1,27 @@
+function addTwo(x,y){
+    answer = x+y
+    console.log(answer)
+}
+addTwo(3,6);
+addTwo(6,12)
+
+function subTwo(x,y){
+    answer= x-y;
+    console.log(answer)
+}
+subTwo(8,4)
+subTwo(16,8)
+
+function multTwo(x,y){
+    answer= x*y;
+    console.log(answer)
+}
+multTwo(4,6)
+multTwo(3,3)
+
+function divTwo(x,y){
+    answer=x/y;
+    console.log(answer)
+}
+    divTwo(4,2)
+    divTwo(10,2)
